356f38c7b7165671a7d2ca9f4e8c7c0feeab14a0
Haoxiang Yang <haoxiangyang89@gmail.com>
2016-10-06 01:03:51 -0500

    Updated Optimizer with the structure of SIP
    The structure of the stochastic integer program is done. Now only the cut generation left.

--- Optimizer/NBA_Stoch_Optimizer.py
+++ Optimizer/NBA_Stoch_Optimizer.py
@@ -15,8 +15,18 @@ from gurobipy import *
 import numpy
 import csv
 
+def getLB(xinc,playerList,pIDList,pData,N,T):
+    lb = 0
+    for i in range(N):
+        pt = 0
+        for item in playerList:
+            pt += pData[i,pIDList.index(item)]*(xinc[item].x)
+        if pt >= T:
+            lb += 1
+    return lb
+
 # the input is the salary file, the simulated scenario file and the player dictionary
-def stoch_opt(salaryF,distrnF,playerDAdd):
+def stoch_opt(salaryF,distrnF,playerDAdd,totalTeam):
     # load the dictionary that maps players' names to their IDs
     playerDict = numpy.load(playerDAdd)[0]
     # load the simulated scenarios
@@ -26,7 +36,7 @@ def stoch_opt(salaryF,distrnF,playerDAdd):
     pData = []
     for item in csvReader:
         if counter == 0:
-            pIDList = item
+            pIDList = item[1:]
         else:
             pData.append(item[1:])
     fi.close()
@@ -41,8 +51,10 @@ def stoch_opt(salaryF,distrnF,playerDAdd):
     posReq = {"C":1,"PG":2,"SG":2,"PF":2,"SF":2}
     
     master = Model()
+    detFirst = Model()
     # define the decision variable
     x = {}
+    xdet = {}
     z = {}
     obj = 0
     # define the objective function
@@ -51,10 +63,15 @@ def stoch_opt(salaryF,distrnF,playerDAdd):
         obj += 1/N*z[i]
     playerList = []
     totalS = 0
+    totalSdet = 0
     posC = {}
+    posCdet = {}
     B = 60000
+    threshold = 360
+    FDPointDet = 0
     for item in posList:
         posC[item] = 0
+        posCdet[item] = 0
     
     for item in csvReader:
         if counter == 0:
@@ -65,15 +82,66 @@ def stoch_opt(salaryF,distrnF,playerDAdd):
             playerID = playerDict[playerName]
             playerList.append(playerID)
             x[playerID] = master.addVar(vtype=GRB.BINARY, name = playerName)
+            xdet[playerID] = detFirst.addVar(vtype = GRB.BINARY, name = playerName + "Det")
             # append the position information
             posC[item[1]] += x[playerID]
+            posCdet[item[1]] += xdet[playerID]
             # append the salary information
             totalS += float(item[6])*x[playerID]
+            totalSdet += float(item[6])*xdet[playerID]
+            # append average points
+            FDPointDet += float(item[4])*xdet[playerID]
     
     fi.close()
     # finish building the master program
     master.update()
+    detFirst.update()
     master.setObjective(obj, GRB.MAXIMIZE)
+    detFirst.setObjective(FDPointDet, GRB.MAXIMIZE)
     master.addConstr(totalS <= B, name = "BudgetConstraint")
+    detFirst.addConstr(totalSdet <= B, name = "BCon_Det")
     for item in posList.keys():
         master.addConstr(posC[item] == posReq[item], name = "PositionConstraint_{}".format(item))
+        detFirst.addConstr(posCdet[item] == posReq[item], name = "PCon_Det_{}".format(item))
+    master.update()
+    detFirst.update()
+    # solve the deterministic expected problem, obtain solutions
+    detFirst.optimize()
+    
+    k = 1
+    l = 1
+    # obtain a lower bound for the current expected solution
+    lb = getLB(xdet,playerList,pIDList,pData,N,threshold)
+    ub = N
+            
+    # generate cuts around the incumbent lower bound solution!!!!!!!!
+    
+    solColl = []
+    for j in range(totalTeam):
+        # start the iteration to solve the decomposed problem!!!!!!!!!
+        while lb < numpy.floor(ub):
+            xSol = []
+            master.optimize()
+            # update the upper bound
+            if ub > master.objVal:
+                ub = master.objVal
+            lb = getLB(x,playerList,pIDList,pData,N,threshold)
+            for iKey in x.keys():
+                if abs(x[iKey].x - 1) <= 0.000001:
+                    xSol.append(iKey)
+            # add the integer L-shaped cuts!!!!!!
+            k += 1
+            master.update()
+            # add the Lagrangian cuts!!!!!!!
+            l += 1
+            master.update()
+        
+        # record the xSol, add the constraint to remove this team, repeat the process
+        solColl.append(xSol)
+        currentT = 0
+        for item in xSol:
+            currentT += x[item]
+        master.addConstr(currentT <= 8)
+        master.update()
+        
+        
